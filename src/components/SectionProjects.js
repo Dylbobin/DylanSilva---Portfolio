61985f023a9e757f4976bfcccebebe01c320a701
Dylbobin <dylansilva22@yahoo.com>
2024-05-18 11:15:43 -0700

    finished project descriptions

--- src/components/SectionProjects.js
+++ src/components/SectionProjects.js
@@ -22,7 +22,7 @@ export const SectionProjects = () => {
                             <img src={pawsitiveProject} />
                             <div className='layer'>
                                 <h3>Pawsitive - iOS App</h3>
-                                <p>swiftUI<br /> rest of desc</p>
+                                <p>swiftUI<br />Senior Capstone Project at California State University Fullerton.  My first large scale project using SwiftUI including various features like MapKit, Firebase/Firestore, Navigation Buttons, User Inputted Dogs, and a clean UI.</p>
                                 <a href='#'><FaExternalLinkAlt onClick={() => window.open("https://github.com/Dylbobin/Pawsitive---SwiftUI")}/></a>
                             </div>
                         </div>
@@ -30,7 +30,7 @@ export const SectionProjects = () => {
                             <img src={taskItProject} />
                             <div className='layer'>
                                 <h3>Task-It - iOS App</h3>
-                                <p>swiftUI<br /> rest of desc</p>
+                                <p>swiftUI<br />Based off of the reminders app on iPhone, Task-It implements basic features of SwiftUI including Navigation links, forms, priority lists, and ability to edit the reminders.</p>
                                 <a href='#'><FaExternalLinkAlt onClick={() => window.open("https://github.com/Dylbobin/Task-It---SwitUI")}/></a>
                             </div>
                         </div>
@@ -38,7 +38,7 @@ export const SectionProjects = () => {
                             <img src={quiznessProject} />
                             <div className='layer'>
                                 <h3>Quizness - Web App</h3>
-                                <p>JS/HTML/CSS<br /> rest of desc</p>
+                                <p>JS/HTML/CSS<br />My first JavaScript project based on making a variety of quizzes, randomizing questions, showing incorrect/correct answers, and providing users with their score.  Very basic, but was a learning project for myself.</p>
                                 <a href='#'><FaExternalLinkAlt onClick={() => window.open("https://github.com/Dylbobin/Quizness")}/></a>
                             </div>
                         </div>
@@ -46,7 +46,7 @@ export const SectionProjects = () => {
                             <img src={basicTodo} />
                             <div className='layer'>
                                 <h3>Basic Todo - Web App</h3>
-                                <p>JS/HTML/CSS<br /> rest of desc</p>
+                                <p>JS/HTML/CSS<br />A basic todo app, this time in React.js.  This was my first put together project in JavaScript with a clean interface and good user experience.</p>
                                 <a href='#'><FaExternalLinkAlt onClick={() => window.open("https://github.com/Dylbobin/Quizness")}/></a>
                             </div>
                         </div>
@@ -54,7 +54,7 @@ export const SectionProjects = () => {
                             <img src={exhaustiveSearch} />
                             <div className='layer'>
                                 <h3>Soccer Problem - Exhaustive Search</h3>
-                                <p>Python<br /> rest of desc</p>
+                                <p>Python<br />Created in algorithms, the soccer problem is the amount of routes you are able to take to reach the goal.</p>
                                 <a href='#'><FaExternalLinkAlt onClick={() => window.open("https://github.com/Dylbobin/Quizness")}/></a>
                             </div>
                         </div>
@@ -62,7 +62,7 @@ export const SectionProjects = () => {
                             <img src={webScraping} />
                             <div className='layer'>
                                 <h3>Newegg - Web Scraping</h3>
-                                <p>Python<br /> rest of desc</p>
+                                <p>Python<br />Using beautiful soup bs4, this webscraper takes in the type of item you would like to scrape off Newegg and finds items within the right criteria and price range.</p>
                                 <a href='#'><FaExternalLinkAlt onClick={() => window.open("https://github.com/Dylbobin/Quizness")}/></a>
                             </div>
                         </div>
